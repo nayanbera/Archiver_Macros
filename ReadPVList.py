75938d84acff94a4f27d9e34982433537b19c6bd
Mrinal Bera <nayanbera@gmail.com>
2019-06-05 09:51:28 -0500

    Edited ReadPVList.py to add All for each individual motor parameters

--- ReadPVList.py
+++ ReadPVList.py
@@ -8,7 +8,7 @@ class ReadPVList(object):
                           'UseEncoder':'UEIP',
                           'MotorRes':'MRES',
                           'EncoderRes':'ERES'}
-        self.data_server='http://chemmat71.cars.aps.anl.gov:17668'
+        self.data_server='http://chemmat104.cars.aps.anl.gov:17668'
         self.webpath=self.data_server+'/retrieval/ui/'
         self.htmlfolder='/opt/archappl/retrieval/webapps/retrieval/ui/'
         self.href = self.data_server+"/retrieval/ui/viewer/archViewer.html?pv="
@@ -56,9 +56,9 @@ class ReadPVList(object):
             self.pvNum=len(self.pvAll)
 
     def create_htmls(self):
-        sidebartxt = '<!-- Sidebar -->\n<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:10%">\n <h2 ' \
-                     'class="w3-bar-item">Categories</h2>\n'
-        for category in self.pvList.keys(): #For creating sidebar
+        sidebartxt = '<!-- Sidebar -->\n<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:20%">\n <h2 ' \
+                     'class="w3-bar-item">Devices</h2>\n'
+        for category in sorted(self.pvList.keys()): #For creating sidebar
             sidebartxt += ' <a href="%s%s.html" class="w3-bar-item w3-button">%s</a>\n' % (self.webpath,category,category)
         sidebartxt +='</div>\n\n'
 
@@ -66,7 +66,7 @@ class ReadPVList(object):
         fh2.writelines(self.headertxt)
         fh2.writelines(sidebartxt)
         fh2.write('\n<!-- Page Content -->\n')
-        fh2.writelines('<div style="margin-left:10%">\n<div class="w3-container w3-teal">\n<h1>ChemMatCARS '
+        fh2.writelines('<div style="margin-left:20%">\n<div class="w3-container w3-teal">\n<h1>ChemMatCARS '
                        'Archiver Appliance</h1></div>\n<div class="w3-container">\n\n')
         fh2.write('<h2>Welcome to ChemMatCARS Archiver</h2>\n')
         fh2.write('<p>Please click on the sidebar items to look into the archived PVs. In order to '
@@ -75,25 +75,26 @@ class ReadPVList(object):
         fh2.writelines(self.footertxt)
         fh2.close()
 
-        for category in self.pvList.keys():
+        for category in sorted(self.pvList.keys()):
             fh=open(self.htmlfolder+category+'.html','w')
 
             fh.writelines(self.headertxt)
             fh.writelines(sidebartxt)
             fh.write('\n<!-- Page Content -->\n')
-            fh.writelines('<div style="margin-left:10%">\n<div class="w3-container w3-teal">\n<h1>ChemMatCARS '
-                          'Archiver Appliance</h1></div>\n<div class="w3-container">\n\n')
+            fh.writelines('<div style="margin-left:20%">\n<div class="w3-container w3-teal">\n<h1>ChemMatCARS '
+                          'Archiver Appliance: '+category+'</h1></div>\n<div class="w3-container">\n\n')
             fh.write('\n')
 
-            motortxt = '<h2>Archived Motor PVs</h2>\n'
-            motortxt +='<table cellspacing = 5>\n'
-            pvtxt = '<h2>Archived PVs</h2>\n'
-            pvtxt += '<table cellspacing = 5>\n'
+            motortxt = '<h3>Archived Motor PVs</h3>\n'
+            motortxt +='<table cellspacing = 10>\n'
+            pvtxt = '<h3>Archived PVs</h3>\n'
+            pvtxt += '<table cellspacing = 10>\n'
             allotxt=''
             onum=0
+            mnum=0
             otherPV=False
             motorPV=False
-            for pv in self.pvList[category].keys():
+            for pv in sorted(self.pvList[category].keys()):
                 if not self.checkMotor[pv]: #For PVs other than motors
                     otherPV=True
                     pvtxt += '<tr>\n'
@@ -106,11 +107,23 @@ class ReadPVList(object):
                     motortxt += '<tr>\n'
                     motortxt += '<td>'+pv+'</td>\n'
                     alltxt=''
-                    for key in self.motorParams.keys():
+                    for key in sorted(self.motorParams.keys()):
                         alltxt+=self.pvList[category][pv][key]+'&pv='
                         motortxt+='<td><a href="'+self.href+self.pvList[category][pv][key]+'">'+key+'</a></td>\n'
                     motortxt += '<td><a href="' + self.href +alltxt[:-4]+ '">All</a></td>\n'
                     motortxt+='</tr>\n\n'
+                    mnum+=1
+            if mnum>0:
+                motortxt+='<tr>\n'
+                motortxt+='<td>All</td>\n'
+                for key in sorted(self.motorParams.keys()):
+                    allsame=''
+                    for pv in sorted(self.pvList[category].keys()):
+                        if self.checkMotor[pv]:
+                            allsame+=self.pvList[category][pv][key]+'&pv='
+                    if allsame!='':
+                    	motortxt+='<td><a href="'+self.href+allsame[:-4]+'">All</a></td>\n'
+                motortxt+='</tr>\n\n'
             if onum>0:
             	pvtxt+='<td><a href="'+self.href+allotxt[:-4]+'">All</a></td>\n'
             motortxt+='</table>\n'
@@ -133,6 +146,5 @@ if __name__=="__main__":
         fh.write(item+'\n')
     fh.close()
     ReadPV.create_htmls()
-
 
 
