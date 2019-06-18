db41cf472d2733803d483a01e497a972dae13988
Mrinal Bera <nayanbera@gmail.com>
2019-06-18 10:56:09 -0500

    Updated Archiver_Viewer with export facility

--- Archiver_Viewer.py
+++ Archiver_Viewer.py
@@ -7,7 +7,9 @@ from PyQt5.uic import loadUi
 from PyQt5.QtCore import QTime, QDate, Qt
 import sys
 from ReadPVList import ReadPVList
-import copy
+import time
+import os
+
 
 class Archiver_Viewer(QMainWindow):
     def __init__(self):
@@ -31,7 +33,8 @@ class Archiver_Viewer(QMainWindow):
         self.init_signals()
 
     def init_signals(self):
-        self.updateDataPushButton.clicked.connect(self.fetchData)
+        self.fetchDataPushButton.clicked.connect(self.fetchData)
+        self.exportDataPushButton.clicked.connect(self.exportData)
 
 
     def update_pvTree(self):
@@ -50,12 +53,14 @@ class Archiver_Viewer(QMainWindow):
                     categ.addChild(apv)
             self.pvTreeWidget.setColumnCount(3)
             self.pvTreeWidget.addTopLevelItem(categ)
+        self.pvTreeWidget.header().setSectionResizeMode(QHeaderView.ResizeToContents)
 
 
     def fetchData(self):
         fr=self.fromDateTimeEdit.dateTime().toUTC().toString(Qt.ISODate)
         to=self.toDateTimeEdit.dateTime().toUTC().toString(Qt.ISODate)
         self.unit={}
+        self.xraw={}
         self.x={}
         self.y={}
         prg_dlg=QProgressDialog(self)
@@ -64,21 +69,33 @@ class Archiver_Viewer(QMainWindow):
         prg_dlg.setMaximum(len(items))
         prg_dlg.setMinimum(0)
         prg_dlg.show()
+        self.datafull={}
         for i, item in enumerate(items):
             pv=item.text(1)
             # url='http://164.54.162.104:17668/retrieval/data/getData.json?pv=15ID:BLEPS:TEMP1_CURRENT&from=2019-06-17T13:20:00%%C2%B10700&to=2019-06-17T14:20:00%%C2%B10700'
             url = 'http://164.54.162.104:17668/retrieval/data/getData.json?pv=%s&from=%s&to=%s' % (pv, fr, to)
             http = urllib3.PoolManager()
             r = http.request('GET', url)
+            # try:
             data = json.loads(r.data)
             self.unit[pv] = data[0]['meta']['EGU']
-            self.x[pv] = pl.array([datetime.datetime.fromtimestamp(item['secs']) for item in data[0]['data']])
+            self.xraw[pv]=pl.array([item['secs'] for item in data[0]['data']])
+            self.x[pv] = pl.array([datetime.datetime.fromtimestamp(item['secs']) for item in data[0]['data']],dtype='datetime64')
             self.y[pv] = pl.array([item['val'] for item in data[0]['data']])
-            prg_dlg.setValue(i+1)
-        self.updatePlot()
+            self.datafull[pv]=pl.vstack((self.xraw[pv],self.y[pv]))
+            # except:
+            #     QMessageBox.warning(self,'Fetch Error','The data from %s cannot be obtained from the server'%pv,QMessageBox.Ok)
+            prg_dlg.setValue(i + 1)
+        if bool(self.unit):
+            self.updatePlot()
 
     def updatePlot(self):
+        try:
+            self.axes2.remove()
+        except:
+            pass
         if len(self.unit.keys())!=2:
+            axis_color='k'
             self.mplWidget.canvas.axes.clear()
             self.mplWidget.canvas.axes.set_xlabel('Time')
             last = None
@@ -88,7 +105,8 @@ class Archiver_Viewer(QMainWindow):
                     ylabel+='%s '%self.unit[pv]
                 self.mplWidget.canvas.axes.step(self.x[pv], self.y[pv],label=pv)
                 last=self.unit[pv]
-            self.mplWidget.canvas.axes.set_ylabel(ylabel)
+            self.mplWidget.canvas.axes.tick_params(axis='y', labelcolor=axis_color)
+            self.mplWidget.canvas.axes.set_ylabel(ylabel,color=axis_color)
             leg=self.mplWidget.canvas.axes.legend(loc='best')
             self.mplWidget.canvas.draw()
         else:
@@ -108,6 +126,19 @@ class Archiver_Viewer(QMainWindow):
             self.axes2.tick_params(axis='y', labelcolor=color)
             self.mplWidget.canvas.draw()
         pl.gcf().autofmt_xdate()
+
+    def exportData(self):
+        if bool(self.unit):
+            self.dir=QFileDialog.getExistingDirectory(self,'Select a directory to save data')
+            for pv in self.unit.keys():
+                fheader='Data saved on %s\n'%(time.asctime())
+                fheader+='col_names=["time[secs]","pv[%s]"]'%self.unit[pv]
+                fname=pv+'_%s.txt'%(time.strftime('%Y%m%d'))
+                fname=os.path.join(self.dir,fname.replace(':','_'))
+                pl.savetxt(fname,self.datafull[pv],header=fheader,comments='#')
+        else:
+            QMessageBox.warning(self,'Data Error','No data fetched to save',QMessageBox.Ok)
+
 
 
 
